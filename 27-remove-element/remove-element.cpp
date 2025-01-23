3b774e53076599e257f7353846e3abb987502732
Tekkali Rahul Lakshmi Subrahmanyam <159406511+Tekrah123@users.noreply.github.com>
2025-01-23 15:35:06 +0530

    Time: 6 ms (0.59%) | Memory: 11.7 MB (71.95%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int index = 0;
-        for(int i = 0; i< nums.size(); i++){
-            if(nums[i] != val){
-                nums[index] = nums[i];
-                index++;
-            }
+    int removeElement(vector<int>& nums, int val) 
+    {
+        nums.erase(remove(nums.begin(), nums.end(),val), nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            cout<<nums[i]<<" ";
         }
-        return index;
+        return nums.size();
     }
+    
 };
